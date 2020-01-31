b3ad5548280149582fc25b5fedc308fad1d78f1d
superterminal <trendemil@gmail.com>
2020-01-31 12:08:17 +0200

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,36 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { graphql } from "gatsby";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+export default ({ data }) => {
+  return (
+    <Layout>
+      <SEO title="home" />
+      <h1>Wordpress blog</h1>
+      <h4>Posts</h4>
+      {data.allWordpressPost.edges.map(({ node }) => (
+        <div>
+            <p>{node.title}</p>
+            <div dangerouslySetInnerHTML={{ __html: node.excerpt}} />
+        </div>
+      ))}
+    </Layout>
+  )
+}
 
-export default IndexPage
+export const pageQuery = graphql`
+  query {
+    allWordpressPost(sort: {fields: [title]}) {
+      edges {
+        node {
+          title
+          excerpt
+          slug
+        }
+      }
+    }
+  }  
+`

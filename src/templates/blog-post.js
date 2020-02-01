efb79b522599c9b109df77ac1f8961ae2561e4af
superterminal <trendemil@gmail.com>
2020-02-01 13:35:46 +0200

    added comments for each blog post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,16 +4,26 @@ import { graphql, Link } from 'gatsby';
 import Img from 'gatsby-image';
 
 import Layout from '../components/layout';
+import { Comments } from './blog-comments';
 
 export default ({ data }) => {
     const post = data.allWordpressPost.edges[0].node;
+    // const comments = Comments();
+    const comments = data.allWordpressWpComments.edges;
     return (
         <Layout>
             <div>
                 <h1>{post.title}</h1>
-                <h6>Published on: {new Date(post.date).toString()}</h6>
+                <h6>Published on: {post.date}</h6>
                 {post.featured_media && <Img resolutions={post.featured_media.localFile.childImageSharp.resolutions} />} 
                 <div dangerouslySetInnerHTML={{ __html: post.content }} />
+                <h2>Comments: </h2>
+                {comments.map(comment => (
+                    <div>
+                        <span>By: {comment.node.author_name}, Date: {comment.node.date}</span> 
+                        <div dangerouslySetInnerHTML={{ __html: comment.node.content}} />
+                    </div>
+                ))}
                 <Link to="/">Back</Link>
             </div>
         </Layout>
@@ -21,13 +31,14 @@ export default ({ data }) => {
 };
 
 export const query = graphql`
-    query($slug: String!) {
+    query($slug: String!, $postId: Int!) {
         allWordpressPost(filter: { slug: { eq: $slug }}) {
             edges {
                 node {
+                    id
                     title
                     content
-                    date
+                    date(formatString: "MMMM DD, YYYY")
                     featured_media {
                         localFile {
                           childImageSharp {
@@ -38,7 +49,17 @@ export const query = graphql`
                             }
                           }
                         }
-                      }
+                    }
+                }
+            }
+        }
+        allWordpressWpComments(filter: {post: {eq: $postId}}) {
+            edges {
+                node {
+                    id
+                    content
+                    author_name
+                    date(formatString: "MMMM DD, YYYY")
                 }
             }
         }

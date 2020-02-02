6ed31f9cc48b79f585ccd9b9fddf1afcf07bb06d
superterminal <trendemil@gmail.com>
2020-02-02 13:41:20 +0200

    added sorting for comments by default, by user still not working

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,33 +4,58 @@ import { graphql, Link } from 'gatsby';
 import Img from 'gatsby-image';
 
 import Layout from '../components/layout';
-import { Comments } from './blog-comments';
 
-export default ({ data }) => {
-    const post = data.allWordpressPost.edges[0].node;
-    // const comments = Comments();
-    const comments = data.allWordpressWpComments.edges;
-    return (
-        <Layout>
-            <div>
-                <h1>{post.title}</h1>
-                <h6>Published on: {post.date}</h6>
-                {post.featured_media && <Img resolutions={post.featured_media.localFile.childImageSharp.resolutions} />} 
-                <div dangerouslySetInnerHTML={{ __html: post.content }} />
-                <h2>Comments: </h2>
-                {comments.map(comment => (
-                    <div key={comment.node.id}>
-                        <span>By: {comment.node.author_name}, Date: {comment.node.date}</span> 
-                        <div dangerouslySetInnerHTML={{ __html: comment.node.content}} />
-                    </div>
-                ))}
-                <Link to="/">Back</Link>
-            </div>
-        </Layout>
-    )
-};
+export default class Posts extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            sortCommentsBy: 'ASC'
+        };
+    }
 
-export const query = graphql`
+    sortChangeHandler = e => {
+        const { value } = e.target;
+        this.setState({
+            sortCommentsBy: value
+        });
+    }
+
+    render() {
+        const post = this.props.data.allWordpressPost.edges[0].node;
+        const comments = this.props.data.allWordpressWpComments.edges;
+       
+        return (
+            <Layout>
+                <div>
+                    <h1>{post.title}</h1>
+                    <h6>Published on: {post.date}</h6>
+                    <h6>Category: {post.categories[0].name}</h6>
+                    {post.featured_media && <Img resolutions={post.featured_media.localFile.childImageSharp.resolutions} />} 
+                    <div dangerouslySetInnerHTML={{ __html: post.content }} />
+                    <hr/>
+                    {comments.length !== 0 ? <div>
+                            <h2>Comments: </h2>
+                            <h3>Sort by: </h3><select onChange={this.sortChangeHandler}>
+                                    <option value="ASC">Ascending</option>
+                                    <option value="DESC">Descending</option>
+                            </select>
+                            {comments.map(comment => (
+                                <div key={comment.node.id}>
+                                    <span>By: {comment.node.author_name}, Date: {comment.node.date}</span>
+                                    <hr/>
+                                    <div dangerouslySetInnerHTML={{ __html: comment.node.content}} />
+                                </div>
+                            ))}
+                    </div> : <h2>No comments yet!</h2>}
+                    
+                    <Link to="/">Back</Link>
+                </div>
+            </Layout>
+        )
+    }
+}
+
+export const pageQuery = graphql`
     query($slug: String!, $postId: Int!) {
         allWordpressPost(filter: { slug: { eq: $slug }}) {
             edges {
@@ -38,7 +63,10 @@ export const query = graphql`
                     id
                     title
                     content
-                    date(formatString: "MMMM DD, YYYY")
+                    date(formatString: "MMMM DD, YYYY, h:mm:ss a")
+                    categories {
+                        name
+                    }
                     featured_media {
                         localFile {
                           childImageSharp {
@@ -53,15 +81,16 @@ export const query = graphql`
                 }
             }
         }
-        allWordpressWpComments(filter: {post: {eq: $postId}}) {
+        allWordpressWpComments(filter: {post: {eq: $postId}}, sort: { fields: date, order: DESC }) {
             edges {
                 node {
                     id
                     content
                     author_name
-                    date(formatString: "MMMM DD, YYYY")
+                    date(formatString: "MMMM DD, YYYY, h:mm:ss a")
                 }
             }
         }
     }
 `
+

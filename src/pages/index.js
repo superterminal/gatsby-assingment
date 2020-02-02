40f6f0977325b502d3ab5e1e5edeb709e26f36b7
superterminal <trendemil@gmail.com>
2020-02-02 19:50:46 +0200

    added post sorting

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,22 +5,48 @@ import Layout from "../components/layout";
 import Image from "../components/image";
 import SEO from "../components/seo";
 
-export default ({ data }) => {
-  return (
-    <Layout>
-      <SEO title="home" />
-      <h1>Wordpress blog</h1>
-      <h4>Latest Posts</h4>
-      {data.allWordpressPost.edges.map(({ node }) => (
-        <div key={node.slug}>
-            <Link to={`/${node.slug}`}>
-              <p>Title: {node.title}</p>
-            </Link>
-            <div dangerouslySetInnerHTML={{ __html: node.excerpt}} />
-        </div>
-      ))}
-    </Layout>
-  )
+import dateSort from '../shared/dateSort';
+
+export default class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sortPostsBy: 'DESC'
+    }
+  }
+
+  sortChangeHandler = e => {
+    const { value } = e.target;
+    this.setState({
+        sortPostsBy: value
+    });
+  }
+
+  render() {
+
+    let posts = this.props.data.allWordpressPost.edges;
+
+    posts = dateSort(posts, this.state.sortPostsBy);
+
+    return (
+      <Layout>
+        <SEO title="home" />
+        <h1>Wordpress blog</h1>
+        <select onChange={this.sortChangeHandler}>
+              <option value="DESC">Latest</option>
+              <option value="ASC">Oldest</option>
+        </select> Posts
+        {posts.map(({ node }) => (
+          <div key={node.slug}>
+              <Link to={`/${node.slug}`}>
+                <p>Title: {node.title}</p>
+              </Link>
+              <div dangerouslySetInnerHTML={{ __html: node.excerpt}} />
+          </div>
+        ))}
+      </Layout>
+    )
+  }
 }
 
 export const pageQuery = graphql`
@@ -31,6 +57,7 @@ export const pageQuery = graphql`
           title
           excerpt
           slug 
+          date(formatString: "MMMM DD, YYYY, h:mm:ss a")
         }
       }
     }
